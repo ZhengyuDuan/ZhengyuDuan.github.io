aa9a274114f459bc93047091fb7175b52343f82f
ZhengyuDuan <670450301@qq.com>
2022-07-08 04:53:06 +0800

    fix logic

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -2,33 +2,18 @@ import cssModule from './Test.module.scss';
 import cx from 'classnames';
 
 export const Test = () => {
+
+	const drops = [];
+	for (let i = 0; i < 110; i++) {
+		drops.push(
+			<div key={i} className={cx(cssModule.raindrop, cssModule.border)} />
+		)
+	}
+
 	return (
 		<div className={cssModule.container} >
 			<div className={cssModule.window} />
-			<div className={cx(cssModule.raindrops)} >
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-				<div className={cssModule.border} />
-				<div className={cssModule.raindrop} />
-			</div>
+			{drops.map(d => { return d })}
 		</div>
 	);
 };
